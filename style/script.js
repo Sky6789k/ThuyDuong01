8b7d9602d2a8446470de48e892c856a332fffebb
khánh <sp.khanhnk@gmail.com>
2025-07-13 15:31:50 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -8,12 +8,12 @@ const sound3 = document.getElementById('bg-music3');
 const sound4 = document.getElementById('bg-music4');
 
 const content = [
-  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Dương chắc chưa? 😢' },
+  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Hiền ơi chắc chưa? 😢' },
   { gif: 'https://i.pinimg.com/originals/c8/07/e2/c807e26d8aed392f172f0bf441f60626.gif', message: 'Thử nghĩ lại nha 🥺' },
   { gif: 'https://i.pinimg.com/originals/0d/ac/7e/0dac7e14010362ff081e2167be218341.gif', message: 'Đừng mà, cho tớ cơ hội đi 💔' },
   { gif: 'https://i.pinimg.com/originals/88/e7/86/88e786492cc527584feee199936813dd.gif', message: 'Thiệt luôn đó hả? 😭' },
-  { gif: 'https://i.pinimg.com/originals/82/be/ae/82beaeb21c686871437f88bbc1593288.gif', message: 'Một lần nữa thôi Mai Ka, năn nỉ đó 😞' },
-  { gif: 'https://i.pinimg.com/originals/97/91/de/9791de11497556c4a5e800427c48fc47.gif', message: 'Tớ buồn đó nha... 😔' },
+  { gif: 'https://i.pinimg.com/originals/82/be/ae/82beaeb21c686871437f88bbc1593288.gif', message: 'Một lần nữa thôi Tí em, năn nỉ đó 😞' },
+  { gif: 'https://i.pinimg.com/originals/97/91/de/9791de11497556c4a5e800427c48fc47.gif', message: 'Tớ buồn đó nha cu bự... 😔' },
 ];
 
 let clickCount = 0;
@@ -25,9 +25,9 @@ noBtn.addEventListener('click', () => {
   clickCount++;
 
   if (clickCount === 3) {
-    noBtn.textContent = 'Bấm Có đi 😭';
+    noBtn.textContent = 'Bấm Có đi Hiền 😭';
   } else if (clickCount === 7) {
-    noBtn.textContent = 'Năn nỉ đó bấm Có đi 😭';
+    noBtn.textContent = 'Năn nỉ đó bấm Có đi mặt lồn😭';
   }
 
   if (clickCount <= 5) {
